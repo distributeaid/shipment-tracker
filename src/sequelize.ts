99d179f0331305613a97f90c6f8023f62258278c
bion <bionjohnson@gmail.com>
2021-02-22 18:36:29 -0800

    Add comment

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,5 +1,8 @@
 import { Sequelize } from 'sequelize-typescript'
 
+// DB_ENV is used soley to set the database config to "ci" for running
+// tests on CI. This is because DB connection config is different between
+// CI and local test envs. See db/config.json
 const env = process.env.DB_ENV || process.env.NODE_ENV || 'development'
 const config = require(__dirname + '/../db/config.json')[env]
 
