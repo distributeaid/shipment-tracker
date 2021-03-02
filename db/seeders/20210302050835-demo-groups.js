d23c251058f31b8e66a041d4580064deb1e3a356
bion <bionjohnson@gmail.com>
2021-03-01 21:45:04 -0800

    Fix the seed

--- db/seeders/20210302050835-demo-groups.js
+++ db/seeders/20210302050835-demo-groups.js
@@ -1,14 +1,34 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Group', [{
-      name: 'Test group',
-      groupType
-    }], {});
+    await queryInterface.bulkInsert(
+      'Groups',
+      [
+        {
+          name: 'Stoke-Upon-Trent',
+          groupType: 'SENDING_GROUP',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Brighton',
+          groupType: 'SENDING_GROUP',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Calais',
+          groupType: 'RECEIVING_GROUP',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+      ],
+      {},
+    )
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Group', null, {});
-  }
-};
+    await queryInterface.bulkDelete('Groups', null, {})
+  },
+}
