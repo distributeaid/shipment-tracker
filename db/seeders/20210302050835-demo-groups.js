ef09e9e2f472b0f6e6a2be3ac0cfc5f9543d9050
Bion <bion@users.noreply.github.com>
2021-03-01 21:43:14 -0800

    Apply suggestions from code review
    Co-authored-by: Maxime Preaux <deammer@users.noreply.github.com>

--- db/seeders/20210302050835-demo-groups.js
+++ db/seeders/20210302050835-demo-groups.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Peoples', [{
+    await queryInterface.bulkInsert('Group', [{
       name: 'Test group',
       groupType
     }], {});
