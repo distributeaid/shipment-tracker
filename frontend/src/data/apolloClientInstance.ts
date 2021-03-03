0c0002471ece2b79fdc8a8c4c31336a3eb7dc871
Maxime Preaux <maxime@forwardxp.com>
2021-03-02 21:44:21 -0800

    Authenticate requests to the backend

--- frontend/src/data/apolloClientInstance.ts
+++ frontend/src/data/apolloClientInstance.ts
@@ -1,8 +1,48 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import {
+  ApolloClient,
+  ApolloLink,
+  fromPromise,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+import { useAuth0 } from '@auth0/auth0-react'
+
+// https://www.apollographql.com/docs/link/links/http/
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_SERVER_URL,
+  credentials: 'include',
+  fetch,
+})
+
+const clientUrl = process.env.REACT_APP_CLIENT_URL
+
+// https://www.apollographql.com/docs/react/networking/authentication/
+const authLink = new ApolloLink((operation, forward) => {
+  const { getAccessTokenSilently } = useAuth0()
+
+  return fromPromise(
+    getAccessTokenSilently()
+      .then((token: string) => {
+        console.log(`Access token: ${token}`)
+        operation.setContext({
+          headers: {
+            authorization: token ? `Bearer ${token}` : '',
+            'Access-Control-Allow-Origin': clientUrl,
+          },
+        })
+
+        return operation
+      })
+      .catch(() => {
+        // not signed in
+        return operation
+      }),
+  ).flatMap(forward)
+})
 
 const apolloClientInstance = new ApolloClient({
-  uri: process.env.REACT_APP_SERVER_URL,
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 })
 
 export default apolloClientInstance
