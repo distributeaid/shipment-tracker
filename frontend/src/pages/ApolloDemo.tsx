abe52dea1b941ef64faa5a707292b21c23f5fd0a
bion <bionjohnson@gmail.com>
2021-03-04 21:08:24 -0800

    Stop polling

--- frontend/src/pages/ApolloDemo.tsx
+++ frontend/src/pages/ApolloDemo.tsx
@@ -1,4 +1,5 @@
 import { useQuery, gql } from '@apollo/client'
+import { useEffect } from 'react'
 
 import LayoutWithNav from '../layouts/LayoutWithNav'
 
@@ -23,9 +24,11 @@ const ApolloDemoPage = () => {
    *    - error: a ApolloError object with some details, in case things fail
    *    - data: the data fetched by your query
    */
-  const { data, loading, error } = useQuery(GROUPS_QUERY, {
+  const { data, loading, error, stopPolling } = useQuery(GROUPS_QUERY, {
     pollInterval: 5000,
   })
+
+  useEffect(() => stopPolling)
 
   /**
    * 3. Render your markup!
