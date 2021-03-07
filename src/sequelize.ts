16508eb2e6aef359eca81836b955a85eb2430df1
bion <bionjohnson@gmail.com>
2021-03-07 10:53:38 -0800

    Update db config for prod

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -4,14 +4,28 @@ import { Sequelize } from 'sequelize-typescript'
 // tests on CI. This is because DB connection config is different between
 // CI and local test envs. See db/config.json
 const env = process.env.DB_ENV || process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../db/config.json')[env]
 
-export const sequelize = new Sequelize({
-  repositoryMode: true,
-  database: config.database,
-  username: config.username,
-  password: config.password,
-  dialect: config.dialect,
-  logging: process.env.NODE_ENV?.toUpperCase() !== 'TEST' && console.log,
-  models: [__dirname + '/models'],
-})
+export let sequelize: Sequelize
+
+if (env === 'production') {
+  if (process.env.DATABASE_URL == null) {
+    throw new Error('DATABASE_URL is null!')
+  }
+
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+  })
+} else {
+  const config = require(__dirname + '/../db/config.json')[env]
+
+  sequelize = new Sequelize({
+    repositoryMode: true,
+    database: config.database,
+    username: config.username,
+    password: config.password,
+    dialect: 'postgres',
+    logging: process.env.NODE_ENV?.toUpperCase() !== 'TEST' && console.log,
+    models: [__dirname + '/models'],
+  })
+}
