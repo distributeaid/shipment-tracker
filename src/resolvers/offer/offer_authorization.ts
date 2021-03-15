6c801d17bb31bd4e72d3c1150ecc4dc1623cdff7
bion <bionjohnson@gmail.com>
2021-03-15 15:45:14 -0700

    Document offer authorizers

--- src/resolvers/offer/offer_authorization.ts
+++ src/resolvers/offer/offer_authorization.ts
@@ -3,6 +3,12 @@ import { AuthenticatedContext } from '../../apolloServer'
 import Offer from '../../models/offer'
 import { OfferStatus, ShipmentStatus } from '../../server-internal-types'
 
+/**
+ * Asserts that an offer has a shipment and sending group.
+ * Throws {@link ApolloError} if missing either
+ *
+ * @param {Offer} offer - the offer check consistency of
+ * */
 const assertAssociations = (offer: Offer): void => {
   if (!offer.sendingGroup) {
     throw new ApolloError(`Offer ${offer.id} has no sending group!`)
@@ -13,6 +19,14 @@ const assertAssociations = (offer: Offer): void => {
   }
 }
 
+/**
+ * Asserts that the authenticated user account in {context} is either an admin or
+ * the captain of the offer's sending group. Throws {@link ForbiddenError} if the
+ * caller is not authorized.
+ *
+ * @param {Offer} offer - the offer to authorize access to
+ * @param {AuthenticatedContext} context - the ApolloServer context containing auth data for the caller
+ * */
 const assertAccountIsCaptainOrAdmin = (
   offer: Offer,
   context: AuthenticatedContext,
@@ -25,6 +39,16 @@ const assertAccountIsCaptainOrAdmin = (
   }
 }
 
+/**
+ * Asserts that the authenticated user account in {context} is authorized to mutate the offer.
+ * Admins may mutate as they wish. The captain of the offer's sending group may mutate when
+ * the offer has status Draft and the shipment has status Open. All other users are unauthorized.
+ *
+ * Throws {@link ForbiddenError} if the caller is not authorized. Throws {@link ApolloError} if the offer is missing essential assocations.
+ *
+ * @param {Offer} offer - the offer to authorize access to
+ * @param {AuthenticatedContext} context - the ApolloServer context containing auth data for the caller
+ * */
 export function authorizeOfferMutation(
   offer: Offer,
   context: AuthenticatedContext,
@@ -47,6 +71,15 @@ export function authorizeOfferMutation(
   }
 }
 
+/**
+ * Asserts that the authenticated user account in {context} is authorized to view the offer.
+ * Admins and captains of the offer's sending group may view. All other users are unauthorized.
+ *
+ * Throws {@link ForbiddenError} if the caller is not authorized. Throws {@link ApolloError} if the offer is missing essential assocations.
+ *
+ * @param {Offer} offer - the offer to authorize access to
+ * @param {AuthenticatedContext} context - the ApolloServer context containing auth data for the caller
+ * */
 export function authorizeOfferQuery(
   offer: Offer,
   context: AuthenticatedContext,
