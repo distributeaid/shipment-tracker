88a412a96780cb45465ae64cfc30520fca44dcb6
Maxime Preaux <maxime@forwardxp.com>
2021-04-14 15:58:17 -0700

    Updated the Auth0 client ID and added docs

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
+import { Auth0Provider } from '@auth0/auth0-react'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Auth0Provider } from '@auth0/auth0-react'
 import App from './AppRoot'
 import './stylesheets/index.output.css'
 
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
       domain="distributeaid.eu.auth0.com"
-      clientId="3wxYeItzvD1fN5tloBikUHED8sQ1BImj"
+      clientId="hfNo3Nw2ZrAGv0Vwh7tjU4nJ7lAuPTNH"
       redirectUri={window.location.origin}
       audience={process.env.REACT_APP_AUTH0_AUDIENCE}
     >
