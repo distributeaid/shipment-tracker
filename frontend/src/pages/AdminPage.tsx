f990e73744d4bcde18df89e919b01dff6c0e6bd1
Maxime Preaux <maxime@forwardxp.com>
2021-05-20 10:15:10 -0700

    Admin page cleanup

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from 'react'
 import { Link } from 'react-router-dom'
+import CogIcon from '../components/icons/CogIcon'
 import LayoutWithNav from '../layouts/LayoutWithNav'
 
 /**
@@ -9,11 +10,18 @@ const AdminPage: FunctionComponent = () => {
   return (
     <LayoutWithNav>
       <div className="bg-white max-w-5xl mx-auto border-l border-r border-gray-200 min-h-content">
-        <h1 className="text-navy-800 text-3xl mb-2">Admin</h1>
-        <div className="text-navy-800 flex flex-col space-y-2">
-          <Link to="/groups">Groups</Link>
-          <Link to="/users">Users (in construction)</Link>
-        </div>
+        <header className="p-6 border-b border-gray-200 md:flex items-center justify-between">
+          <h1 className="text-navy-800 text-3xl mb-4 md:mb-0">Admin</h1>
+        </header>
+        <main className="p-4">
+          <div className="text-navy-800 flex flex-col space-y-2">
+            <Link to="/groups" className="flex items-center hover:underline">
+              <CogIcon className="w-5 h-5 mr-1" />
+              Manage groups
+            </Link>
+            <Link to="/users">Users (in construction)</Link>
+          </div>
+        </main>
       </div>
     </LayoutWithNav>
   )
