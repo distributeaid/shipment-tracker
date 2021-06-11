413c3e76c05c4ea70f3c5ba4d995c67f7f06edf1
Markus Tacker <m@coderbyheart.com>
2021-06-11 21:59:27 +0200

    feat: add input validation for adding a group (#152)
    * feat: add input validation for adding a group
    
    This adds input validation to the addGroup mutation,
    using typebox to define a JSON schema, and Ajv to
    validate the input against the schema.
    
    See #47
    
    * Added line breaks to make code more readable
    
    Co-authored-by: Maxime Preaux <maxime@forwardxp.com>

---
+++ src/tests/validateWithJSONSchema.test.ts
@@ -0,0 +1,40 @@
+import { Type } from '@sinclair/typebox'
+import { validateWithJSONSchema } from '../resolvers/validateWithJSONSchema'
+
+describe('validateWithJSONSchema', () => {
+  it('should validate input against a JSON schema', () => {
+    expect(
+      validateWithJSONSchema(
+        Type.Object({
+          foo: Type.String({ minLength: 1 }),
+        }),
+      )({ foo: 'bar' }),
+    ).toEqual({
+      value: { foo: 'bar' },
+      errors: undefined,
+    })
+  })
+
+  it('should return errors if input does not match the JSON schema', () => {
+    expect(
+      validateWithJSONSchema(
+        Type.Object({
+          foo: Type.String({ minLength: 1 }),
+        }),
+      )({ bar: 'baz' }),
+    ).toEqual({
+      value: undefined,
+      errors: [
+        {
+          instancePath: '',
+          keyword: 'required',
+          message: "must have required property 'foo'",
+          params: {
+            missingProperty: 'foo',
+          },
+          schemaPath: '#/required',
+        },
+      ],
+    })
+  })
+})
