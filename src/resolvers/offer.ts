83beaafc4e6c0030a6a549c2b65d2230b7d78a9f
Markus Tacker <m@coderbyheart.com>
2021-06-13 23:05:31 +0200

    feat(offer): add input validation to offer resolver

--- src/resolvers/offer.ts
+++ src/resolvers/offer.ts
@@ -1,5 +1,5 @@
+import { Type } from '@sinclair/typebox'
 import { ForbiddenError, UserInputError } from 'apollo-server'
-import { has } from 'lodash'
 import Group from '../models/group'
 import Offer, { OfferAttributes } from '../models/offer'
 import Pallet from '../models/pallet'
@@ -11,105 +11,144 @@ import {
   QueryResolvers,
   ShipmentStatus,
 } from '../server-internal-types'
+import { Contact } from './input-validation/Contact'
+import { validateIdInput } from './input-validation/idInputSchema'
+import { ID, URI } from './input-validation/types'
+import { validateWithJSONSchema } from './input-validation/validateWithJSONSchema'
 import {
   authorizeOfferMutation,
   authorizeOfferQuery,
 } from './offer_authorization'
-import validateEnumMembership from './validateEnumMembership'
-import validateUris from './validateUris'
+
+// Offer mutation resolvers
+
+// - add offer
+
+export const addOfferInput = Type.Object(
+  {
+    sendingGroupId: ID,
+    shipmentId: ID,
+    contact: Contact,
+    photoUris: Type.Optional(Type.Array(URI)),
+  },
+  { additionalProperties: false },
+)
+
+const validateAddOfferInput = validateWithJSONSchema(addOfferInput)
 
 const addOffer: MutationResolvers['addOffer'] = async (
   _parent,
   { input },
   context,
 ) => {
-  if (!input.sendingGroupId || !input.shipmentId) {
-    throw new UserInputError('Offer arguments invalid')
+  const valid = validateAddOfferInput(input)
+  if ('errors' in valid) {
+    throw new UserInputError('Add offer arguments invalid', valid.errors)
   }
 
-  validateUris(input.photoUris || [])
-
-  const sendingGroupPromise = Group.findByPk(input.sendingGroupId)
-  const shipmentPromise = Shipment.findByPk(input.shipmentId)
+  const sendingGroupPromise = Group.findByPk(valid.value.sendingGroupId)
+  const shipmentPromise = Shipment.findByPk(valid.value.shipmentId)
   const existingOfferCountPromise = Offer.count({
     where: {
-      sendingGroupId: input.sendingGroupId,
-      shipmentId: input.sendingGroupId,
+      sendingGroupId: valid.value.sendingGroupId,
+      shipmentId: valid.value.sendingGroupId,
     },
   })
 
   const sendingGroup = await sendingGroupPromise
   if (sendingGroup?.captainId !== context.auth.userAccount.id) {
     throw new ForbiddenError(
-      `User ${context.auth.userAccount.id} not permitted to create offer for group ${input.sendingGroupId}`,
+      `User ${context.auth.userAccount.id} not permitted to create offer for group ${valid.value.sendingGroupId}`,
     )
   }
 
   const shipment = await shipmentPromise
   if (shipment == null) {
-    throw new UserInputError(`Shipment ${input.shipmentId} does not exist`)
+    throw new UserInputError(
+      `Shipment ${valid.value.shipmentId} does not exist`,
+    )
   }
 
   if (shipment.status !== ShipmentStatus.Open) {
     throw new UserInputError(
-      `Shipment ${input.shipmentId} is not accepting offers`,
+      `Shipment ${valid.value.shipmentId} is not accepting offers`,
     )
   }
 
   const existingOfferCount = await existingOfferCountPromise
   if (existingOfferCount > 0) {
     throw new UserInputError(
-      `Shipment ${input.shipmentId} already has offer from group ${input.sendingGroupId}`,
+      `Shipment ${valid.value.shipmentId} already has offer from group ${valid.value.sendingGroupId}`,
     )
   }
 
   return Offer.create({
+    ...valid.value,
     status: OfferStatus.Draft,
     statusChangeTime: new Date(),
-    contact: input.contact,
-    shipmentId: input.shipmentId,
-    sendingGroupId: input.sendingGroupId,
-    photoUris: input.photoUris || [],
   })
 }
+
+// - update offer
+
+export const updateOfferInput = Type.Object(
+  {
+    id: ID,
+    status: Type.Enum(OfferStatus),
+    contact: Type.Optional(Contact),
+    photoUris: Type.Optional(Type.Array(URI)),
+  },
+  { additionalProperties: false },
+)
+
+const validateUpdateOfferInput = validateWithJSONSchema(updateOfferInput)
 
 const updateOffer: MutationResolvers['updateOffer'] = async (
   _parent,
   { input },
   context,
 ) => {
-  const offer = await Offer.findByPk(input.id, {
+  const valid = validateUpdateOfferInput(input)
+  if ('errors' in valid) {
+    throw new UserInputError('Update offer arguments invalid', valid.errors)
+  }
+
+  const offer = await Offer.findByPk(valid.value.id, {
     include: [{ association: 'sendingGroup' }, { association: 'shipment' }],
   })
 
   if (!offer) {
-    throw new UserInputError(`Offer ${input.id} does not exist`)
+    throw new UserInputError(`Offer ${valid.value.id} does not exist`)
   }
 
   authorizeOfferMutation(offer, context)
 
   const updateAttributes: Partial<OfferAttributes> = {}
 
-  if (input.status) {
-    validateEnumMembership(OfferStatus, input.status)
-    updateAttributes.status = input.status
+  if (valid.value.status) {
+    updateAttributes.status = valid.value.status
     updateAttributes.statusChangeTime = new Date()
   }
 
-  if (has(input, 'contact')) {
-    updateAttributes.contact = input.contact
+  if (input.contact !== undefined) {
+    updateAttributes.contact = valid.value.contact
   }
 
-  if (has(input, 'photoUris')) {
-    const uris = input.photoUris || []
-    validateUris(uris)
-    updateAttributes.photoUris = uris
+  if (input.photoUris !== undefined) {
+    updateAttributes.photoUris = valid.value.photoUris
   }
 
   return offer.update(updateAttributes)
 }
 
+// Offer query resolvers
+
 const offer: QueryResolvers['offer'] = async (_, { id }, context) => {
+  const valid = validateIdInput({ id })
+  if ('errors' in valid) {
+    throw new UserInputError('Offer arguments invalid', valid.errors)
+  }
+
   const offer = await Offer.findByPk(id, {
     include: [{ association: 'sendingGroup' }, { association: 'shipment' }],
   })
@@ -128,6 +167,18 @@ const listOffers: QueryResolvers['listOffers'] = async (
   { shipmentId },
   context,
 ) => {
+  const valid = validateWithJSONSchema(
+    Type.Object(
+      {
+        shipmentId: ID,
+      },
+      { additionalProperties: false },
+    ),
+  )({ shipmentId })
+  if ('errors' in valid) {
+    throw new UserInputError('Offer arguments invalid', valid.errors)
+  }
+
   const groupsPromise = Group.findAll({
     where: { captainId: context.auth.userAccount.id },
   })
