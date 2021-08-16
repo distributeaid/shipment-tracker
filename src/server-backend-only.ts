5dcdd36a00b7a2b5291bb57e13b19f2926e7405b
Markus Tacker <m@coderbyheart.com>
2021-08-16 23:03:24 +0200

    feat: add backend only server

---
+++ src/server-backend-only.ts
@@ -0,0 +1,39 @@
+import cors from 'cors'
+import express from 'express'
+import { createServer } from 'http'
+import apolloServer from './apolloServer'
+import findOrCreateProfile from './findOrCreateProfile'
+import sendShipmentExportCsv from './sendShipmentExportCsv'
+import './sequelize'
+
+const app = express()
+
+app.get('/profile', findOrCreateProfile)
+app.get('/shipment-exports/:id', sendShipmentExportCsv)
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN || 'http://localhost:8080',
+    credentials: true,
+  }),
+)
+
+async function startExpressServer() {
+  await apolloServer.start()
+
+  apolloServer.applyMiddleware({
+    app,
+    path: '/graphql',
+    cors: false, // We use the cors plugin for this
+  })
+}
+
+startExpressServer()
+
+const httpServer = createServer(app)
+
+const port = process.env.PORT || 3000
+
+httpServer.listen({ port }, (): void =>
+  console.log(`Listening on port ${port}`),
+)
