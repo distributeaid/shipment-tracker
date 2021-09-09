44b8fc395a8b9e2723f0f4dbc6c07433d0e67872
Markus Tacker <m@coderbyheart.com>
2021-09-09 22:44:27 +0200

    build: use local rclone

--- .clevercloud/publish-frontend-to-cellar.sh
+++ .clevercloud/publish-frontend-to-cellar.sh
@@ -5,6 +5,8 @@ export RCLONE_CONFIG_MYS3_SECRET_ACCESS_KEY=$CELLAR_ADDON_KEY_SECRET
 export RCLONE_CONFIG_MYS3_ENDPOINT=$CELLAR_ADDON_HOST
 export RCLONE_CONFIG_MYS3_TYPE="s3"
 
-curl https://rclone.org/install.sh | bash
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+cd rclone-*-linux-amd64
 
-rclone sync ./frontend/build mys3:$CELLAR_BUCKET.$CELLAR_ADDON_HOST --progress --s3-acl=public-read
+rclone sync ../frontend/build mys3:$CELLAR_BUCKET.$CELLAR_ADDON_HOST --progress --s3-acl=public-read
