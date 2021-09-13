cf5a097700bc4746b14abdb88efd417c838845be
Markus Tacker <m@coderbyheart.com>
2021-09-13 23:33:11 +0200

    feat: include version string

--- .clevercloud/publish-frontend-to-cellar.sh
+++ .clevercloud/publish-frontend-to-cellar.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+cd ..
+pwd
+
 export RCLONE_CONFIG_MYS3_ACCESS_KEY_ID=$CELLAR_ADDON_KEY_ID
 export RCLONE_CONFIG_MYS3_SECRET_ACCESS_KEY=$CELLAR_ADDON_KEY_SECRET
 export RCLONE_CONFIG_MYS3_ENDPOINT=$CELLAR_ADDON_HOST
@@ -9,9 +12,14 @@ curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip rclone-current-linux-amd64.zip
 cd rclone-*-linux-amd64
 
+cd frontend
+export REACT_APP_VERSION=$COMMIT_ID
+yarn build
+cd ..
+
 echo "Uploading site to bucket $CELLAR_BUCKET ..."
 
-./rclone sync ../frontend/build mys3:$CELLAR_BUCKET --progress --s3-acl=public-read
+./rclone sync ./frontend/build mys3:$CELLAR_BUCKET --progress --s3-acl=public-read
 
 echo "Done. Site should be available on https://$CELLAR_BUCKET.$CELLAR_ADDON_HOST now."
 echo "Configured Origin: $ORIGIN"
