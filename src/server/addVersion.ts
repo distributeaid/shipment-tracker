6e145eada4c2e0ac32815e678d0b716d213fe34d
Markus Tacker <m@coderbyheart.com>
2021-09-14 00:24:36 +0200

    fix: remove log statement

--- src/server/addVersion.ts
+++ src/server/addVersion.ts
@@ -3,6 +3,5 @@ import { NextFunction, Request, Response } from 'express'
 export const addVersion =
   (version: string) => (_: Request, res: Response, next: NextFunction) => {
     res.header('X-Shipment-Tracker-Backend-Version', version)
-    console.log(res)
     next()
   }
