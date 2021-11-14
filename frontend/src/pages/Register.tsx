01b01c4d0396a4de5e319c3e4b75868b501ba78d
Maxime Preaux <hello@maxime.dev>
2021-11-14 12:50:54 -0800

    Added a checkbox to reveal the password

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent, useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import Error from '../components/alert/Error'
-import Button from '../components/Button'
+import CheckboxField from '../components/forms/CheckboxField'
 import FormFooter from '../components/forms/FormFooter'
 import TextField from '../components/forms/TextField'
 import InternalLink from '../components/InternalLink'
@@ -14,7 +14,6 @@ const RegisterPage: FunctionComponent = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [password2, setPassword2] = useState('')
   const [showPassword, setShowPassword] = useState(false)
   const [error, setError] = useState<AuthError>()
   const [isRegistered, setIsRegistered] = useState<boolean>(false)
@@ -22,7 +21,6 @@ const RegisterPage: FunctionComponent = () => {
   const isFormValid =
     emailRegEx.test(email) &&
     passwordRegEx.test(password) &&
-    password === password2 &&
     name.trim().length > 0
 
   return (
@@ -51,66 +49,21 @@ const RegisterPage: FunctionComponent = () => {
             value={email}
             onChange={({ target: { value } }) => setEmail(value)}
           />
-          <div className="flex flex-row gap-x-6">
-            <div className="flex-auto space-y-6 items-center">
-              <TextField
-                label="Pick a good password"
-                type={showPassword ? 'text' : 'password'}
-                name="password"
-                autoComplete="new-password"
-                minLength={8}
-                value={password}
-                onChange={({ target: { value } }) => setPassword(value)}
-              />
-              <TextField
-                label="Repeat your password"
-                type={showPassword ? 'text' : 'password'}
-                name="password2"
-                autoComplete="new-password"
-                minLength={8}
-                value={password2}
-                onChange={({ target: { value } }) => setPassword2(value)}
-              />
-            </div>
-            <div className="flex-none">
-              <Button
-                aria-label="show password"
-                aria-pressed={showPassword}
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden={true}
-                    className="h-8 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
-                      clipRule="evenodd"
-                    />
-                    <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden={true}
-                    className="h-8 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                    <path
-                      fillRule="evenodd"
-                      d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                )}
-              </Button>
-            </div>
+          <div>
+            <TextField
+              label="Pick a good password"
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              autoComplete="new-password"
+              minLength={8}
+              value={password}
+              onChange={({ target: { value } }) => setPassword(value)}
+            />
+            <CheckboxField
+              containerClassName="mt-2"
+              label="Reveal password"
+              onChange={() => setShowPassword((prev) => !prev)}
+            />
           </div>
           <FormFooter>
             <button
