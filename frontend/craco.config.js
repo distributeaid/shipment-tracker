73e643e73ea4a9313bdcedc825dc7cc124ddd1f4
Markus Tacker <m@coderbyheart.com>
2022-01-19 22:41:10 +0000

    feat: print deployed version

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,4 +1,10 @@
-// https://tailwindcss.com/docs/guides/create-react-app
+// otherwise homepage from package.json will be used
+process.env.PUBLIC_URL = process.env.PUBLIC_URL ?? '/'
+
+// COMMIT_ID is defined on Clever Cloud deployments
+process.env.REACT_APP_VERSION = process.env.COMMIT_ID
+
+// https://github.com/gsoft-inc/craco/blob/master/packages/craco/README.md#configuration-file
 module.exports = {
   style: {
     postcss: {
