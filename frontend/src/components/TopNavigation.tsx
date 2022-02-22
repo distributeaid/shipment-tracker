55f293878d9b7700af92e8592959fc122ce20dc9
Markus Tacker <m@coderbyheart.com>
2022-02-22 23:05:57 +0000

    fix(onboarding): hide shipment links for users without groups

--- frontend/src/components/TopNavigation.tsx
+++ frontend/src/components/TopNavigation.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent, ReactNode } from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
+import { useGroupLeaderGroups } from '../hooks/useGroupLeaderGroups'
 import ROUTES from '../utils/routes'
 import Badge from './Badge'
 import DistributeAidLogo from './branding/DistributeAidLogo'
@@ -18,52 +19,47 @@ export interface NavLinkItem {
   adminOnly?: boolean
 }
 
-const NAV_LINKS: NavLinkItem[] = [
-  {
-    path: ROUTES.SHIPMENT_LIST,
-    label: 'Shipments',
-    icon: <TruckIcon className="w-5 h-5 mr-2" />,
-  },
-  {
-    path: ROUTES.ADMIN_ROOT,
-    label: 'Admin',
-    icon: <CogIcon className="w-5 h-5 mr-2" />,
-    adminOnly: true,
-  },
-]
-
-interface Props {
+/**
+ * A full-width element that sits at the top of the page. It displays the DA
+ * branding and a dropdown-menu with some account information.
+ */
+const TopNavigation: FunctionComponent<{
   /**
    * If true, the user's information and "log out" button will be hidden.
    * Use this prop when you want to show the page header while things are
    * still loading.
    */
   hideControls?: boolean
-}
-
-/**
- * A full-width element that sits at the top of the page. It displays the DA
- * branding and a dropdown-menu with some account information.
- */
-const TopNavigation: FunctionComponent<Props> = ({ hideControls }) => {
+}> = ({ hideControls }) => {
   const { logout, me: profile } = useAuth()
-
+  const userHasGroups = useGroupLeaderGroups().length > 0
   const userIsAdmin = profile?.isAdmin
 
-  const filteredNavLinks = NAV_LINKS.filter((link) =>
-    link.adminOnly ? userIsAdmin : true,
-  )
+  const navLinks: NavLinkItem[] = []
+  if (userIsAdmin)
+    navLinks.push({
+      path: ROUTES.ADMIN_ROOT,
+      label: 'Admin',
+      icon: <CogIcon className="w-5 h-5 mr-2" />,
+    })
+
+  if (userHasGroups)
+    navLinks.push({
+      path: ROUTES.SHIPMENT_LIST,
+      label: 'Shipments',
+      icon: <TruckIcon className="w-5 h-5 mr-2" />,
+    })
 
   return (
     <header className="py-2 bg-navy-800 h-nav sticky top-0 z-20">
       <div className="max-w-5xl px-4 mx-auto h-full flex items-center justify-between">
-        <MobileNavigation navLinks={filteredNavLinks} />
+        <MobileNavigation navLinks={navLinks} />
         <div className="flex items-center">
           <Link to="/" className="text-white" aria-label="Go to the home page">
             <DistributeAidLogo className="block h-8" />
           </Link>
         </div>
-        <DesktopNavigation navLinks={filteredNavLinks} />
+        <DesktopNavigation navLinks={navLinks} />
         {!hideControls && profile && (
           <div className="flex items-center text-white">
             <DropdownMenu
